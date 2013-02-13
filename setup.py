4f3784e763843a563e48c9fd0447df004f093bce
Julozi <julien.seiler@gmail.com>
2013-02-13 15:54:16 +0100

    Defined setup config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pybonita',
+    version='0.1',
+    author='Tony Moutaux <moutaux@igbmc.fr>, Julien Seiler <julien.seiler@gmail.com>',
+    packages=['pybonita'],
+    description='',
+    long_description='',
+    license='BSD License',
+    install_requires=['requests']
+)
