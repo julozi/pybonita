97751a2de1bd9345e31e1e3b001778564a82c6ac
Julozi <julien.seiler@gmail.com>
2013-02-13 16:54:13 +0100

    Sample implementation

--- pybonita/__init__.py
+++ pybonita/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['BonitaObject', 'BonitaServer']
+__all__ = ['server', 'BonitaObject']
+
+server = BonitaServer()
 
 class BonitaObject:
     """ All Bonita's entities inherit from BonitaObject """
@@ -15,4 +17,27 @@ class BonitaServer:
     Bonita REST server
 
     """
-    pass
+
+    host = None
+    username = None
+    password = None
+
+    def __init__(self):
+        pass
+
+    def connect(host, username, password):
+        self.host = host
+        self.username = username
+        self.password = password
+
+    def sendRESTRequest(self, url, data, user):
+
+        data = dict()
+        data['options'] = u"user:%s" % user
+        # add data from method params
+
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        response = requests.post(host+url, data=data, headers=headers, auth=HTTPBasicAuth(self.username, self.password))
+
+        if response.status_code != requests.codes.ok:
+            raise Exception
--- pybonita/process.py
+++ pybonita/process.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from pybonita import server
+
 __all__ = ['BonitaProcess']
 
 class BonitaProcess(BonitaObject):
 
-    pass
+    def __init__(self, uuid):
+        super(self, BonitaObject).__init__(uuid)
+
+    def instanciate(self, user="john"):
+
+        url = "/bonita-server-rest/API/runtimeAPI/instantiateProcess/%s" % self.uuid
+
+        server.sendRESTRequest(url, None, user)
