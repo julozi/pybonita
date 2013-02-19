53ccf6cdc8bf5072daa18c087d136e67ab93681f
Julozi <julien.seiler@gmail.com>
2013-02-19 11:35:54 +0100

    Refactor BonitaServer class to make it a clean singleton

--- pybonita/__init__.py
+++ pybonita/__init__.py
@@ -12,8 +12,6 @@ logger = logging.getLogger(__name__)
 class BonitaObject(object):
     """ All Bonita's entities inherit from BonitaObject """
 
-    server = None
-
     def __init__(self, uuid):
         self.uuid = uuid
 
@@ -29,7 +27,7 @@ class BonitaObject(object):
         (url,data) = self._generate_save_url(variables)
 
         # Call the BonitaServer
-        xml = self.server.sendRESTRequest(url=url, user=user, data=data)
+        xml = BonitaServer.get_instance().sendRESTRequest(url=url, user=user, data=data)
 
         # Extact UUID of newly created object
         dom = parseString(xml)
@@ -46,7 +44,7 @@ class BonitaObject(object):
         (url,data) = self._generate_delete_url()
 
         # Call the BonitaServer
-        xml = self.server.sendRESTRequest(url = url, user=user, data=data)
+        xml = BonitaServer.get_instance().sendRESTRequest(url = url, user=user, data=data)
 
         #TODO Test return code for completion
 
@@ -64,55 +62,146 @@ class BonitaObject(object):
         """
         raise NotImplementedError
 
+class _MetaBonitaServer(type):
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, BonitaServer._BonitaServerImpl)
 
 class BonitaServer:
-    """
-    A BonitaServer instance is required to establish a connection with a
-    Bonita REST server
+    """ Singleton object to declare Bonita server to be used
+
+
+    BonitaServer is a singleton object used by all BonitaObject within the
+    pybonita package.
+
+    You can't instanciate BonitaServer.
+    To get the unique BonitaServer instance please use the static get_instance
+    method.
 
     """
 
-    host = None
-    username = None
-    password = None
+    __metaclass__ = _MetaBonitaServer
 
-    user = u"john"
+    class _BonitaServerImpl(object):
+
+        def __init__(self):
+            self._user = "john"
+            self._host = None
+            self._port = None
+            self._login = None
+            self._password = None
+            self._ready = False
+
+        def sendRESTRequest(self, url, user=None, data=dict()):
+
+            if self._ready == False:
+                raise Exception #FIXME: raise BonitaServerError here
+
+            user = user if user != None else self._user
+
+            if data is None:
+                data = dict()
+            elif type(data) != dict:
+                raise TypeError
+
+            data['options'] = u"user:%s" % user
+
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            full_url = 'http://%s:%s/bonita-server-rest/API%s' % (self.host, self.port, url)
+
+            response = requests.post(full_url, data=data, headers=headers, auth=HTTPBasicAuth(self.login, self.password))
+
+            if response.status_code != requests.codes.ok:
+                #FIXME Should raise an Exception
+                print response.text
+
+            return response.text
+
+        def _get_host(self):
+            return self._host
+
+        def _set_host(self, value):
+            if type(value) != str:
+                raise TypeError(u"host must be a string")
+
+            self._host = value
+
+        host = property(_get_host, _set_host, None, u"Bonita REST API server host")
+
+        def _get_port(self):
+            return self._port
+
+        def _set_port(self, value):
+            if type(value) != int:
+                raise TypeError(u"port must be an integer")
+
+            self._port = value
+
+        port = property(_get_port, _set_port, None, u"Bonita REST API server port")
+
+        def _get_login(self):
+            return self._login
+
+        def _set_login(self, value):
+            if type(value) != str:
+                raise TypeError(u"login must be a string")
+
+            self._login = value
+
+        login = property(_get_login, _set_login, None, u"Bonita REST request credential login")
+
+        def _get_password(self):
+            return self._password
+
+        def _set_password(self, value):
+
+            if type(value) != str:
+                raise TypeError
+
+            self._password = value
+
+        password = property(_get_password, _set_password, None, u"Bonita REST request credential password")
 
     @classmethod
-    def connect(cls, host, port, username, password):
-        """
-        Instanciate a BonitaServer object and define it as the server singleton
-        for all BonitaObject
+    def get_instance(cls):
+        """ Returns the singleton instance of BonitaServer
+
+        Upon its first call, it creates a new instance of the BonitaServer
+        implementation class
+        On all subsequent calls, the already created instance is returned.
 
         """
+        try:
+            return cls._instance
+        except AttributeError:
+            cls._instance = cls._BonitaServerImpl()
+            return cls._instance
 
-        #fixme: we should check that the host is responding or raise an
-        #exception
+    @classmethod
+    def use(cls, host, port, login, password):
+        """ Set the connexion params to the BonitaServer
 
-        BonitaObject.server = BonitaServer(host, port, username, password)
+        Returns the unique BonitaServer instance
 
-    def __init__(self, host, port, username, password):
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
+        :param host: Bonita REST API server host
+        :type host: str
+        :param port: Bonita REST API server port
+        :type port: int
+        :param login: Bonita REST request credential login
+        :type login: str
+        :param password: Bonita REST request credential password
+        :type password: str
 
-    def sendRESTRequest(self, url, user=None, data=dict()):
+        """
 
-        user = user if user != None else self.user
+        server = cls.get_instance()
+        server.host = host
+        server.port = port
+        server.login = login
+        server.password = password
+        server._ready = True
 
-        if type(data) != dict:
-            data = dict()
+        return server
 
-        data['options'] = u"user:%s" % user
-
-        headers = {'content-type': 'application/x-www-form-urlencoded'}
-        full_url = 'http://%s:%s/bonita-server-rest/API%s' % (self.host, self.port, url)
-
-        response = requests.post(full_url, data=data, headers=headers, auth=HTTPBasicAuth(self.username, self.password))
-
-        if response.status_code != requests.codes.ok:
-            #FIXME Should raise an Exception also
-            print response.text
-
-        return response.text
+    def __init__(self):
+        raise TypeError('BonitaServer must be accessed through `get_instance()`.')
--- pybonita/process.py
+++ pybonita/process.py
@@ -2,7 +2,7 @@
 
 from xml.dom.minidom import parseString
 
-from pybonita import BonitaObject, logger
+from pybonita import BonitaObject, BonitaServer, logger
 from pybonita.utils import dictToMapString
 
 __all__ = ['BonitaProcess']
@@ -12,17 +12,41 @@ class BonitaProcess(BonitaObject):
     def __init__(self, uuid):
         super(BonitaProcess, self).__init__(uuid)
 
-    def instanciate(self, user="john", variables=None):
+    @classmethod
+    def get(cls, uuid):
+        return BonitaProcess(uuid)
+
+
+class BonitaCase(BonitaObject):
+
+    @classmethod
+    def get(cls, uuid):
+        case = super(BonitaCase, self).__init__(uuid)
+
+        url = "/queryRuntimeAPI/getProcessInstance/%s" % self.uuid
+
+        xml = BonitaServer.get_instance().sendRESTRequest(url=url)
+
+        print xml
+
+        return case
+
+    def __init__(self, process, variables=None):
+
+        self._process = process
+        self._variables = variables
+
+    def start(self, user=None):
 
         data = dict()
 
-        if variables == None:
-            url = "/runtimeAPI/instantiateProcess/%s" % self.uuid
+        if self._variables == None:
+            url = "/runtimeAPI/instantiateProcess/%s" % self._process.uuid
         else:
-            url = "/runtimeAPI/instantiateProcessWithVariables/%s" % self.uuid
-            data['variables'] = dictToMapString(variables)
+            url = "/runtimeAPI/instantiateProcessWithVariables/%s" % self._process.uuid
+            data['variables'] = dictToMapString(self._variables)
 
-        xml = self.server.sendRESTRequest(url=url, user=user, data=data)
+        xml = BonitaServer.get_instance().sendRESTRequest(url=url, user=user, data=data)
 
         dom = parseString(xml)
         process_instances = dom.getElementsByTagName("ProcessInstanceUUID")
@@ -34,20 +58,7 @@ class BonitaProcess(BonitaObject):
 
         uuid = values[0].childNodes[0].data
 
-        return BonitaCase(uuid)
-
-
-class BonitaCase(BonitaObject):
-
-    def __init__(self, uuid):
-        super(BonitaCase, self).__init__(uuid)
-
-        url = "/queryRuntimeAPI/getProcessInstance/%s" % self.uuid
-
-        xml = self.server.sendRESTRequest(url=url)
-
-        print xml
-
+        self.uuid = uuid
 
     def _generate_delete_url(self):
         """ Generate URL and data to used to call Bonita server to perform a
